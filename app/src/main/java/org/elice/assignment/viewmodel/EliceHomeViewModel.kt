f65c1c790aae2ec3f3e618f98e9d749f8dee52c9
casio2978 <casio2978@naver.com>
2024-02-24 12:01:23 +0900

    feat: HomeViewModel 구현

---
+++ app/src/main/java/org/elice/assignment/viewmodel/EliceHomeViewModel.kt
@@ -0,0 +1,46 @@
+package org.elice.assignment.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import org.elice.assignment.domain.entities.CourseEntity
+import org.elice.assignment.domain.usecase.course.GetEliceCourseList
+
+class EliceHomeViewModel(
+    private val getEliceCourseList: GetEliceCourseList
+) : ViewModel() {
+
+    private val _eliceCourseList: MutableStateFlow<List<CourseEntity>> =
+        MutableStateFlow(mutableListOf())
+    val eliceCourseList: StateFlow<List<CourseEntity>> = _eliceCourseList
+
+    private val _homeState: MutableStateFlow<EliceHomeUiState> =
+        MutableStateFlow(EliceHomeUiState.LOADING)
+    val homeState: StateFlow<EliceHomeUiState> = _homeState
+
+    init {
+        loadCourses()
+    }
+
+    private fun loadCourses() {
+        viewModelScope.launch {
+            getEliceCourseList(
+                offset = 0,
+                count = 10,
+                filterIsRecommended = null,
+                filterIsFree = null
+            ).collectLatest { courseList ->
+                _eliceCourseList.value = courseList
+                _homeState.value = EliceHomeUiState.SUCCESS
+            }
+        }
+    }
+}
+
+enum class EliceHomeUiState {
+    LOADING,
+    SUCCESS
+}
