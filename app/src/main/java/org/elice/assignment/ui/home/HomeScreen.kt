d4fe1ced7e6395e1c92a32ea3c4007407ed788d0
casio2978 <casio2978@naver.com>
2024-02-24 18:37:55 +0900

    design: API call Success UI 상태 반영

--- app/src/main/java/org/elice/assignment/ui/home/HomeScreen.kt
+++ app/src/main/java/org/elice/assignment/ui/home/HomeScreen.kt
@@ -3,17 +3,21 @@ package org.elice.assignment.ui.home
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import org.elice.assignment.domain.entities.CourseEntity
+import org.elice.assignment.domain.entities.createMockCourseEntity
 import org.elice.assignment.ui.theme.AssignmentTheme
 import org.elice.assignment.viewmodel.EliceHomeUiState
 import org.elice.assignment.viewmodel.EliceHomeViewModel
@@ -24,35 +28,45 @@ fun HomeScreen(
     homeViewModel: EliceHomeViewModel = hiltViewModel()
 ) {
     val eliceHomeUiState by homeViewModel.homeState.collectAsStateWithLifecycle()
-    val eliceCourseList by homeViewModel.eliceCourseList.collectAsState()
+    val eliceFreeCourseList by homeViewModel.eliceFreeCourseList.collectAsState()
+    val eliceRecommendedCourseList by homeViewModel.eliceRecommendedCourseList.collectAsState()
 
     HomeContent(
         eliceHomeUiState = eliceHomeUiState,
-        eliceCourseList = eliceCourseList
+        eliceFreeCourseList = eliceFreeCourseList,
+        eliceRecommendedList = eliceRecommendedCourseList
     )
 }
 
 @Composable
 internal fun HomeContent(
     eliceHomeUiState: EliceHomeUiState,
-    eliceCourseList: List<CourseEntity>
+    eliceFreeCourseList: List<CourseEntity>,
+    eliceRecommendedList: List<CourseEntity>
 ) {
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
     ) {
-        HomeHeader()
-
-        CourseGridList()
-
         when (eliceHomeUiState) {
             EliceHomeUiState.LOADING -> {
                 // todo loading
             }
 
             EliceHomeUiState.SUCCESS -> {
-                Log.d("API CALL", eliceCourseList.toString())
+                Log.d("API CALL", eliceFreeCourseList.toString())
+
+                HomeHeader()
+                CourseGridList(
+                    courseList = eliceFreeCourseList,
+                    title = "무료 과목",
+                )
+                Spacer(Modifier.padding(vertical = 8.dp))
+                CourseGridList(
+                    courseList = eliceRecommendedList,
+                    title = "추천 과목",
+                )
             }
         }
     }
@@ -64,7 +78,8 @@ fun HomePreview() {
     AssignmentTheme {
         HomeContent(
             eliceHomeUiState = EliceHomeUiState.SUCCESS,
-            eliceCourseList = listOf()
+            eliceFreeCourseList = createMockCourseEntity(),
+            eliceRecommendedList = createMockCourseEntity()
         )
     }
 }
