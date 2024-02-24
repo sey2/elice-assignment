6e812d12921e64982e105337f3ff025614420d93
casio2978 <casio2978@naver.com>
2024-02-24 16:43:42 +0900

    design: Font 설정

--- app/src/main/java/org/elice/assignment/ui/theme/Type.kt
+++ app/src/main/java/org/elice/assignment/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package org.elice.assignment.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.elice.assignment.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -15,20 +17,12 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+)
+
+val NotoBold = FontFamily(
+    Font(R.font.noto_700)
+)
+
+val NotoRegular = FontFamily(
+    Font(R.font.noto_500)
 )
