e2f766ce69c80b6406cc31cdb2dcdca7bf78bdc3
casio2978 <casio2978@naver.com>
2024-02-24 09:42:53 +0900

    chore: SDK 버전 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "org.elice.assignment"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "org.elice.assignment"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
