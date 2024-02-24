e84ca0f7f2d161365a16ef3bcb2c47ec60035821
casio2978 <casio2978@naver.com>
2024-02-24 18:36:54 +0900

    feat: CourseEntity MockData 메서드 구현

--- app/src/main/java/org/elice/assignment/domain/entities/EliceCourseListEntity.kt
+++ app/src/main/java/org/elice/assignment/domain/entities/EliceCourseListEntity.kt
@@ -1,5 +1,7 @@
 package org.elice.assignment.domain.entities
 
+import com.google.gson.annotations.SerializedName
+
 data class EliceCourseListEntity(
     val courses: List<CourseEntity>,
     val courseCount: Int
@@ -8,8 +10,20 @@ data class EliceCourseListEntity(
 data class CourseEntity(
     val id: Int,
     val title: String,
+    @SerializedName("short_description")
     val shortDescription: String,
     val isRecommended: Boolean,
     val isFree: Boolean,
     val logoFileUrl: String?
+)
+
+fun createMockCourseEntity(): List<CourseEntity> = listOf(
+    CourseEntity(
+        9999,
+        "C언어 챌린지C언어 챌린지C언어 챌린지",
+        "나의 C언어 실력을 테스트 해보세요!나의 C언어 실력을 테스트 해보세요!",
+        false,
+        false,
+        "mock"
+    )
 )
