dff9279059b4c98fbd3c36294a2fa886f976e168
casio2978 <casio2978@naver.com>
2024-02-24 09:34:26 +0900

    design: Home, CourseDetail 화면 추가

---
+++ app/src/main/java/org/elice/assignment/detail/CourseDetailScreen.kt
@@ -0,0 +1,21 @@
+package org.elice.assignment.detail
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+
+@Composable
+internal fun CourseDetailScreen(navHostController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Text(modifier = Modifier, text = "CourseDetail")
+    }
+}
---
+++ app/src/main/java/org/elice/assignment/home/HomeScreen.kt
@@ -0,0 +1,21 @@
+package org.elice.assignment.home
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+
+@Composable
+internal fun HomeScreen(navHostController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ) {
+        Text(modifier = Modifier, text = "Home")
+    }
+}
