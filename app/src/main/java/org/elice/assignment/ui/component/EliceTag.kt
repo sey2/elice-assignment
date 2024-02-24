9b0648918681dbec3d3fc0abc4802f8760c11414
casio2978 <casio2978@naver.com>
2024-02-24 16:44:22 +0900

    design: Tag Component 구현

---
+++ app/src/main/java/org/elice/assignment/ui/component/EliceTag.kt
@@ -0,0 +1,34 @@
+package org.elice.assignment.ui.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.elice.assignment.ui.theme.NotoBold
+
+@Composable
+fun EliceTag(
+    modifier: Modifier = Modifier,
+    text: String = ""
+) {
+    Surface(
+        modifier = Modifier.padding(top = 8.dp, end = 4.dp),
+        color = Color(0xFFE4E4E4),
+        shape = RoundedCornerShape(4.dp)
+    ) {
+        Text(
+            text = text,
+            modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+            fontSize = 8.sp,
+            fontFamily = NotoBold,
+            color = Color.Black,
+            textAlign = TextAlign.Center
+        )
+    }
+}
