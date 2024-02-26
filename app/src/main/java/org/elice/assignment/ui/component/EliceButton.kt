0c61aa46ad6d29ef21795c1a1c5e4f2b098681e9
casio2978 <casio2978@naver.com>
2024-02-27 01:37:32 +0900

    refactor: 버튼 컴포넌트화

---
+++ app/src/main/java/org/elice/assignment/ui/component/EliceButton.kt
@@ -0,0 +1,43 @@
+package org.elice.assignment.ui.component
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.elice.assignment.ui.theme.ElicePurple
+import org.elice.assignment.ui.theme.EliceRed
+import org.elice.assignment.ui.theme.NotoBold
+
+@Composable
+fun EliceButton(
+    modifier: Modifier,
+    activateText: String = "",
+    deActivateText: String = "",
+    isActivate: Boolean,
+    onClick: () -> Unit = {},
+) {
+    Button(
+        modifier = modifier,
+        onClick = { onClick() },
+        shape = RoundedCornerShape(10.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = if (isActivate) EliceRed else ElicePurple,
+            contentColor = MaterialTheme.colorScheme.onPrimary
+        )
+    ) {
+        Text(
+            text = if (isActivate) deActivateText else activateText,
+            fontSize = 16.sp,
+            fontFamily = NotoBold,
+            lineHeight = 24.sp,
+            modifier = Modifier
+                .padding(start = 8.dp)
+        )
+    }
+}
