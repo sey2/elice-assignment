f29d4196847182007996d97a3492ff840287a299
casio2978 <casio2978@naver.com>
2024-02-26 15:16:59 +0900

    feat: CourseDetailEntity 필요한 인자 추가

--- app/src/main/java/org/elice/assignment/domain/entities/EliceCourseDetailEntity.kt
+++ app/src/main/java/org/elice/assignment/domain/entities/EliceCourseDetailEntity.kt
@@ -5,6 +5,7 @@ import com.google.gson.annotations.SerializedName
 data class CourseDetailEntity(
     val id: Int,
     val title: String,
+    val description: String?,
     @SerializedName("image_file_url")
     val imageFileUrl: String?,
     @SerializedName("logo_file_url")
